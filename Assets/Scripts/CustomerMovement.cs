6b843e018db6ad7fa3dba1fd7643218e6df18c37
pranav5612517 <s112711@ltisdschools.net>
2025-04-03 13:04:00 -0500

    new thing

--- Assets/Scripts/CustomerMovement.cs
+++ Assets/Scripts/CustomerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,18 @@ using UnityEngine;
 public class CustomerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float scaleSpeed = 1f; // Adjust for desired scaling speed
+    public float targetScale = 100f; // Target scale value
+
+    private Vector3 initialScale;
+
     void Start()
     {
-        
+        initialScale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = Vector3.Lerp(transform.localScale, initialScale * targetScale, scaleSpeed * Time.deltaTime);
     }
 }
