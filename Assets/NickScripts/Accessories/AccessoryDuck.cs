bae2a4397ef33913c988eec54e27ae3f5b73bd24
CoffeeeDrinker <s109445@ltisdschools.net>
2025-04-09 13:24:38 -0500

    hi

--- Assets/NickScripts/Accessories/AccessoryDuck.cs
+++ Assets/NickScripts/Accessories/AccessoryDuck.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class AccessoryDuck : MonoBehaviour
 {
+    List<ItemNames> items;
     // Start is called before the first frame update
     void Start()
     {
-        
+        items = new List<ItemNames>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Item") {
+            items.Add(collision.transform.GetComponent<AccessoryItem>().GetItem());
+            Debug.Log(collision.transform.GetComponent<AccessoryItem>().GetItem());
+        }
     }
 }
--- Assets/NickScripts/Accessories/AccessoryItem.cs
+++ Assets/NickScripts/Accessories/AccessoryItem.cs
@@ -7,6 +7,8 @@ public class AccessoryItem : MonoBehaviour, Clickable
     private bool isHeld;
     private Vector3 offset;
     private Rigidbody2D rigidbody;
+    [SerializeField] ItemNames itemType;
+    private bool isStuck;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,11 +28,17 @@ public class AccessoryItem : MonoBehaviour, Clickable
         }
     }
 
+    public ItemNames GetItem() {
+        return itemType;
+    }
     void Drag() {
         rigidbody.MovePosition(offset+Camera.main.ScreenToWorldPoint(Input.mousePosition));
     }
 
     public void OnClick() {
+        if(isStuck) {
+            return;
+        }
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.GetComponent<Rigidbody2D>().gravityScale = 0;
         isHeld = true;
@@ -39,9 +47,20 @@ public class AccessoryItem : MonoBehaviour, Clickable
     }
 
     public void OnRelease() {
+        if(isStuck) {
+            return;
+        }
         isHeld = false;
         transform.GetComponent<Rigidbody2D>().gravityScale = 1;
         rigidbody.includeLayers = LayerMask.GetMask("Shelf");
         rigidbody.excludeLayers = LayerMask.GetMask("Line");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.transform.tag == "Duck") {
+            rigidbody.gravityScale = 0;
+            GetComponent<BoxCollider2D>().enabled = false;
+            isStuck = true;
+        }
     }
 }
