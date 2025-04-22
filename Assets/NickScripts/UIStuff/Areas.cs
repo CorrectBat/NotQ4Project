65f67e0cc0712b1c7b69c9dde7f723f1ebdbdbe2
CoffeeeDrinker <s109445@ltisdschools.net>
2025-04-22 14:19:29 -0500

    work?

---
+++ Assets/NickScripts/UIStuff/Areas.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Areas : MonoBehaviour
+{
+    [SerializeField] GameObject[] areas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchOff() {
+        foreach(GameObject area in areas) {
+            area.SetActive(false);
+        }
+    }
+
+    public void SwitchOn(GameObject area) {
+        area.SetActive(true);
+    }
+}
