30bb54cddcf43bae68c6a870f00619daff202819
CoffeeeDrinker <s109445@ltisdschools.net>
2025-05-08 13:05:01 -0500

    ??

---
+++ Assets/Scripts/ButtonRevealer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonRevealer : MonoBehaviour
+{
+    public GameObject buttonToReveal; // Assign the button in the inspector
+    public float revealDelay = 5f; // Set the time in seconds
+
+    void Start()
+    {
+        if (buttonToReveal != null)
+        {
+            buttonToReveal.SetActive(false); // Initially hide the button
+            Invoke("RevealButton", revealDelay); // Call RevealButton after the delay
+        }
+        else
+        {
+            Debug.LogWarning("ButtonToReveal not assigned. Please assign in the inspector.");
+        }
+    }
+
+    void RevealButton()
+    {
+        if (buttonToReveal != null)
+        {
+            buttonToReveal.SetActive(true); // Make the button visible
+        }
+    }
+}
